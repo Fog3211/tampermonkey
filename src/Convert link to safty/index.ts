2b778a0d07d6503b60b5b95274d09b23a13dd483
Fog3211 <fog3211@163.com>
2021-08-09 23:55:33 +0800

    first commit

---
+++ src/Convert link to safty/index.ts
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         去除简书、知乎外链安全限制
+// @namespace    http://tampermonkey.net/
+// @version      0.1.1
+// @description  去除简书、知乎外链安全限制
+// @author       Fog3211
+// @include      https://www.jianshu.com/*
+// @include      https://www.zhihu.com/*
+// @include      https://zhuanlan.zhihu.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  const ConfigList = [
+    { targetReg: /https:\/\/\w+.jianshu.com/, linkReg: /https:\/\/link.jianshu.com\/\?t=/ },
+    { targetReg: /https:\/\/\w+.zhihu.com/, linkReg: /https:\/\/link.zhihu.com\/\?target=/ }
+  ]
+  const record = ConfigList.find(u => u.targetReg.test(window.location.href))
+  if (record) {
+    const aLists = document.querySelectorAll('a')
+    if (aLists) {
+      aLists.forEach(elm => {
+        if (record.linkReg.test(elm.href)) {
+          const targetUrl = elm.href.replace((record.linkReg), '')
+          elm.setAttribute('href', decodeURIComponent(targetUrl))
+        }
+      })
+    }
+  }
+})();
