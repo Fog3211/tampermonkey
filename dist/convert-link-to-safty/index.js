06b99ff521df24d0c4cc4ed7827de9f54c48975f
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-01-04 10:03:01 +0800

    chore: update desc

--- dist/convert-link-to-safty/index.js
+++ dist/convert-link-to-safty/index.js
@@ -2,8 +2,8 @@
 // ==UserScript==
 // @name         去除简书、知乎、掘金外链安全限制
 // @namespace    https://github.com/Fog3211/tampermonkey/blob/main/dist/convert-link-to-safty/index.js
-// @version      0.1.3
-// @description  去除简书、知乎、掘金外链安全限制
+// @version      0.1.4
+// @description  去除简书、知乎、掘金外链安全限制，将a标签改为直接跳转
 // @author       Fog3211
 // @match      https://*.jianshu.com/*
 // @match      https://*.zhihu.com/*
