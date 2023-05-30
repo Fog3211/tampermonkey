281454b7da75ef28814bb2b004671256b70536df
Fog3211 <Fog3211@users.noreply.github.com>
2023-05-30 02:06:46 +0000

    deploy: 21950281b62d075fdf62411a9b8633497be321f3

--- convert-link-to-safty.js
+++ convert-link-to-safty.js
@@ -2,31 +2,38 @@
 // ==UserScript==
 // @name         去除简书、知乎、掘金外链安全限制
 // @namespace    https://raw.githubusercontent.com/Fog3211/tampermonkey/gh-pages/convert-link-to-safty.js
-// @version      0.1.11
+// @version      0.2.0
 // @description  去除简书、知乎、掘金外链安全限制，将a标签改为直接跳转
 // @author       Fog3211
 // @match      https://*.jianshu.com/*
 // @match      https://*.zhihu.com/*
 // @match      https://*.juejin.cn/*
 // @match      https://*.sspai.com/*
+// @match      https://*.csdn.net/*
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 (function () {
     'use strict';
     var loading = false;
+    var ConfigList = [
+        // https://www.jianshu.com/p/c5e07343515d
+        { key: 'jianshu', linkSelector: ['//link.jianshu.com', '//links.jianshu.com'], splitFlag: 'to=' },
+        { key: 'zhihu', linkSelector: ['//link.zhihu.com'], splitFlag: 'target=' },
+        { key: 'juejin', linkSelector: ['//link.juejin.cn'], splitFlag: 'target=' },
+        { key: 'sspai', linkSelector: ['https://sspai.com'], splitFlag: 'target=' },
+        { key: 'csdn', linkSelector: ['https://blog.csdn.net'], splitFlag: 'target=', searchKey: 'target' }
+    ];
+    var getSearchParams = function (url, key) {
+        if (!url)
+            return null;
+        return new URL(url).searchParams.get(key);
+    };
     var rewriteHref = function () {
         if (loading) {
             return;
         }
         loading = true;
-        var ConfigList = [
-            // https://www.jianshu.com/p/c5e07343515d
-            { key: 'jianshu', linkSelector: ['//link.jianshu.com', '//links.jianshu.com'], splitFlag: 'to=' },
-            { key: 'zhihu', linkSelector: ['//link.zhihu.com'], splitFlag: 'target=' },
-            { key: 'juejin', linkSelector: ['//link.juejin.cn'], splitFlag: 'target=' },
-            { key: 'sspai', linkSelector: ['https://sspai.com'], splitFlag: 'target=' }
-        ];
         var record = ConfigList.find(function (u) { return window.location.hostname.includes(u.key); });
         if (record) {
             var aLists = Array.from(document.querySelectorAll(record.linkSelector.map(function (u) { return "a[href*='".concat(u, "']"); }).join(',')));
@@ -46,6 +53,16 @@
         }
         loading = false;
     };
+    var redirectUrl = function () {
+        var record = ConfigList.find(function (u) { return window.location.hostname.includes(u.key); });
+        if (record === null || record === void 0 ? void 0 : record.searchKey) {
+            var targetUrl = getSearchParams(window.location.href, record.searchKey);
+            if (targetUrl) {
+                window.location.href = targetUrl;
+            }
+        }
+    };
+    redirectUrl();
     rewriteHref();
     setInterval(function () {
         rewriteHref();
