c8fa208b594a686178bcf09428f60f22bbc0264e
Fog3211 <Fog3211@users.noreply.github.com>
2024-09-18 15:32:15 +0000

    deploy: 95665610555dfc398c99548ce9d0e58a7b8785e6

--- external-link-resolver.js
+++ external-link-resolver.js
@@ -1,20 +1,23 @@
 "use strict";
 // ==UserScript==
-// @name         Universal External Link Direct Redirect
+// @name         去除网站外链跳转安全限制
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  Process external links on various websites to enable direct redirection to target sites
+// @version      1.0.1
+// @description  去除简书、知乎、掘金、CSDN、思否、简书等网站的外链安全限制，将a标签改为直接跳转
 // @match        https://juejin.cn/*
 // @match        https://link.juejin.cn/*
 // @match        https://segmentfault.com/*
 // @match        https://link.segmentfault.com/*
+// @match        https://csdn.net/*
+// @match        https://link.csdn.net/*
+// @match        https://*.jianshu.com/*
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 (function () {
     "use strict";
     const siteConfigs = {
         "juejin": {
-            directMatch: "link.juejin.cn",
+            directMatch: ["link.juejin.cn"],
             linkSelector: "a[href^=\"https://link.juejin.cn\"]",
             extractTarget: async (url) => {
                 const params = new URLSearchParams(url.search);
@@ -29,10 +32,9 @@
             }
         },
         "segmentfault": {
-            directMatch: "link.segmentfault.com",
+            directMatch: ["link.segmentfault.com"],
             linkSelector: "a[href^=\"https://link.segmentfault.com\"]",
             extractTarget: async (url) => {
-                // TODO: Optimize SegmentFault link processing logic, consider more efficient methods
                 return new Promise((resolve) => {
                     GM_xmlhttpRequest({
                         method: "GET",
@@ -54,16 +56,24 @@
             }
         },
         "csdn": {
-            directMatch: "link.csdn.net",
+            directMatch: ["link.csdn.net"],
             linkSelector: "a[href^=\"https://link.csdn.net/\"]",
             extractTarget: (url) => {
                 const params = new URLSearchParams(url.search);
                 return params.get("target");
             }
+        },
+        "jianshu": {
+            directMatch: ["link.jianshu.com", "links.jianshu.com"],
+            linkSelector: "a[href^=\"https://link.jianshu.com\"], a[href^=\"https://links.jianshu.com\"]",
+            extractTarget: (url) => {
+                const params = new URLSearchParams(url.search);
+                return params.get("to");
+            }
         }
     };
     async function processRule(config) {
-        if (window.location.hostname === config.directMatch) {
+        if (config.directMatch.includes(window.location.hostname)) {
             let targetUrl = await config.extractTarget(new URL(window.location.href));
             while (targetUrl && targetUrl.startsWith("https://link.juejin.cn")) {
                 // Keep resolving until we get the final non-Juejin link
@@ -95,8 +105,7 @@
         observer.observe(document.body, { childList: true, subtree: true });
     }
     async function init() {
-        const currentSite = Object.keys(siteConfigs).find(site => window.location.hostname.includes(site) ||
-            window.location.hostname.includes(siteConfigs[site].directMatch));
+        const currentSite = Object.keys(siteConfigs).find(site => siteConfigs[site].directMatch.includes(window.location.hostname));
         if (currentSite) {
             const config = siteConfigs[currentSite];
             await processRule(config);
